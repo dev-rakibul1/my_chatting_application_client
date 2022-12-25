2153edc855d9e66ee8d093a3061cd32afbef9940
dev-rakibul1 <fl.rakibul@gmail.com>
2022-12-25 19:33:32 +0600

    Right bar design done

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,134 @@
+import { Add, MoreVert } from "@mui/icons-material";
 import "./Rightbar.css";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="right-bar-wrapper">
+        <h4 className="right-bar-birthday-title">Birthdays</h4>
+        <div className="birthday-container">
+          <img src="/assets/gift.png" alt="" className="birthday-image" />
+          <span className="birthday-text">
+            <strong>Alina</strong> and 3 other people birthday today.
+          </span>
+        </div>
+
+        {/* add */}
+        {/* <img src="/assets/ad.png" alt="add" className="add-image" /> */}
+        {/* active friend */}
+        <hr className="right-bar-hr" />
+        <div className="right-bar-active-friend-wrapper">
+          <h4 className="right-bar-active-title">Active friends</h4>
+          <h4>
+            <MoreVert className="right-active-bar-icons" />
+          </h4>
+        </div>
+        <ul className="right-bar-friend-list">
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/6.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+              <div className="right-bar-online"></div>
+            </div>
+            <div className="right-bar-username">John Doe</div>
+          </li>
+
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/7.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+              <div className="right-bar-online"></div>
+            </div>
+            <div className="right-bar-username">Jel fin</div>
+          </li>
+
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/8.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+              <div className="right-bar-online"></div>
+            </div>
+            <div className="right-bar-username">Hoy Tom</div>
+          </li>
+
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/9.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+              <div className="right-bar-online"></div>
+            </div>
+            <div className="right-bar-username">Jerry bin</div>
+          </li>
+
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/10.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+              <div className="right-bar-online"></div>
+            </div>
+            <div className="right-bar-username">Ketrin</div>
+          </li>
+        </ul>
+        <hr className="right-bar-hr" />
+        {/* group conversations */}
+        <h4 className="right-bar-active-title">Group conversations</h4>
+        <ul className="right-bar-friend-list">
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/8.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+            </div>
+            <div className="right-bar-username">
+              International Remote Jobs for CSE, EEE and IT Students
+            </div>
+          </li>
+
+          {/* active friend 01 */}
+          <li className="right-bar-friend">
+            <div className="right-bar-profile-fn-img">
+              <img
+                src="/assets/person/7.jpeg"
+                className="right-bar-fn-img"
+                alt="active friend"
+              />
+            </div>
+            <div className="right-bar-username">English Spoken</div>
+          </li>
+
+          <li className="right-bar-create-new-group">
+            <Add className="create-new-group-icon" />{" "}
+            <span className="create-new-group-text">
+              Create new conversations
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
