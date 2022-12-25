c4eca81c10b624a2dbdc82d597cd6df6b9a8bd48
dev-rakibul1 <fl.rakibul@gmail.com>
2022-12-26 00:17:44 +0600

    Liked add done

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,20 @@
-import { MoreVert, Public } from "@mui/icons-material";
+import { MoreVert, Public, Textsms, ThumbUp } from "@mui/icons-material";
+import { useState } from "react";
 import { Users } from "../../dummyData";
 import "./Post.css";
 
 const Post = ({ post }) => {
   const { desc, photo, like, comment, date, userId } = post;
 
-  // const userData = Users.filter((user) => user.id === 1);
-  // console.log(userData[0].username);
+  const [liked, setLiked] = useState(like);
+  const [isLiked, setIsLiked] = useState(false);
+  const [thumbLikeColor, SetThumbLikeColor] = useState(false);
+
+  const likeHandle = () => {
+    setLiked(isLiked ? liked - 1 : liked + 1);
+    setIsLiked(!isLiked);
+    SetThumbLikeColor(!thumbLikeColor);
+  };
 
   return (
     <div className="post">
@@ -48,12 +56,31 @@ const Post = ({ post }) => {
           <div className="post-bottom-left">
             <img className="like-icon" src="/assets/like.png" alt="like" />
             <img className="like-icon" src="/assets/heart.png" alt="heart" />
-            <span className="post-like-counter">{like}</span>
+            <span className="post-like-counter">{liked}</span>
           </div>
           <div className="post-bottom-right">
             <span className="post-comments">
               {comment} {comment <= 1 ? " Comment" : " Comments"}
             </span>
+          </div>
+        </div>
+
+        <hr className="like-comment-hr" />
+
+        {/* like comment handler */}
+        <div className="like-comment-handler">
+          <div
+            className={
+              thumbLikeColor ? `like-handler liked-color` : `like-handler`
+            }
+            onClick={likeHandle}
+          >
+            <ThumbUp className="like-thumb-icon" />
+            <span className="like-thumb-text">Like</span>
+          </div>
+          <div className="comment-handler">
+            <Textsms className="comment-icon" />
+            <span className="comment-text">Comment</span>
           </div>
         </div>
       </div>
