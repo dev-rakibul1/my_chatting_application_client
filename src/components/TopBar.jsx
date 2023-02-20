5161f02a50f1e15a565e91f57613751342648dc5
dev-rakibul1 <fl.rakibul@gmail.com>
2023-02-21 00:29:38 +0600

    message Notifications complete

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,17 +1,31 @@
-import { Chat, Home, Notifications, Person, Search } from "@mui/icons-material";
+import {
+  Chat,
+  Home,
+  MoreVert,
+  Notifications,
+  Person,
+  Public,
+  Search,
+} from "@mui/icons-material";
 import "./TopBar.css";
 
 import Logout from "@mui/icons-material/Logout";
 import PersonAdd from "@mui/icons-material/PersonAdd";
+import SearchIcon from "@mui/icons-material/Search";
 import Settings from "@mui/icons-material/Settings";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
+import Fade from "@mui/material/Fade";
 import IconButton from "@mui/material/IconButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
 import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import PopupState, { bindPopper, bindToggle } from "material-ui-popup-state";
 import * as React from "react";
 import { Link } from "react-router-dom";
 
@@ -74,10 +88,174 @@ const TopBar = () => {
           </div>
 
           {/* person */}
-          <div className="topBarIconItem">
-            <Chat />
-            <span className="topBar-icon-badge">3</span>
-          </div>
+          <PopupState
+            variant="popper"
+            popupId="demo-popup-popper"
+            style={{ boxShadow: "1px 2px 1pc #000" }}
+          >
+            {(popupState) => (
+              <div>
+                <Box {...bindToggle(popupState)}>
+                  <div className="topBarIconItem">
+                    <Chat />
+                    <span className="topBar-icon-badge">3</span>
+                  </div>
+                </Box>
+                <Popper {...bindPopper(popupState)} transition>
+                  {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={350}>
+                      <Paper style={{ maxWidth: "320px" }}>
+                        <Box
+                          sx={{
+                            p: 2,
+                            backgroundColor: "#fff",
+                          }}
+                        >
+                          {/* user chat title */}
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                              Chats
+                            </Typography>
+                            <Typography variant="span">
+                              <IconButton
+                                color="green"
+                                aria-label="add to shopping cart"
+                              >
+                                <MoreVert />
+                              </IconButton>
+                            </Typography>
+                          </Box>
+
+                          {/* chat search */}
+                          <Box className="chat-search-wrap">
+                            <input
+                              type="text"
+                              placeholder="Search friends"
+                              className="chat-search"
+                            />
+
+                            <SearchIcon className="chat-search-icon" />
+                          </Box>
+                        </Box>
+
+                        {/* friend 01 */}
+                        <Box
+                          className="poster-title-img"
+                          sx={{
+                            p: 2,
+                            borderBottom: "1px solid #ddd",
+                            cursor: "pointer",
+                          }}
+                        >
+                          <img
+                            src="/assets/person/2.jpeg"
+                            alt="post-profile"
+                            className="post-top-profile"
+                          />
+                          <Box>
+                            <Typography
+                              variant="span"
+                              sx={{
+                                fontSize: "16px",
+                                fontWeight: "600",
+                                textTransform: "capitalize",
+                              }}
+                              className="user-post-name"
+                            >
+                              John Doe
+                            </Typography>
+                            <Typography sx={{ fontSize: "13px" }}>
+                              Hi, May I talk to you...?
+                            </Typography>
+
+                            {/* Message time */}
+                            <Typography
+                              variant="span"
+                              sx={{
+                                fontSize: "11px",
+                                fontWeight: 600,
+                                display: "flex",
+                                justifyContent: "start",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Public
+                                sx={{
+                                  fontSize: "11px",
+                                  marginRight: "3px",
+                                }}
+                              />{" "}
+                              <span>4m ago</span>
+                            </Typography>
+                          </Box>
+                        </Box>
+
+                        {/* friend 02 */}
+                        <Box
+                          className="poster-title-img"
+                          sx={{
+                            p: 2,
+                            borderBottom: "1px solid #ddd",
+                            cursor: "pointer",
+                          }}
+                        >
+                          <img
+                            src="/assets/person/1.jpeg"
+                            alt="post-profile"
+                            className="post-top-profile"
+                          />
+                          <div>
+                            <Typography
+                              variant="span"
+                              sx={{
+                                fontSize: "16px",
+                                fontWeight: "600",
+                                textTransform: "capitalize",
+                              }}
+                              className="user-post-name"
+                            >
+                              Safak Kocaoglu
+                            </Typography>
+                            <Typography sx={{ fontSize: "13px" }}>
+                              Lorem ipsum, dolor sit amet co...
+                            </Typography>
+
+                            {/* Message time */}
+                            <Typography
+                              variant="span"
+                              sx={{
+                                fontSize: "11px",
+                                fontWeight: 600,
+                                display: "flex",
+                                justifyContent: "start",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Public
+                                sx={{
+                                  fontSize: "11px",
+                                  marginRight: "3px",
+                                }}
+                              />{" "}
+                              <span>13m ago</span>
+                            </Typography>
+                          </div>
+                        </Box>
+                      </Paper>
+                    </Fade>
+                  )}
+                </Popper>
+              </div>
+            )}
+          </PopupState>
+
+          {/* *********************** */}
 
           {/* person */}
           <div className="topBarIconItem">
